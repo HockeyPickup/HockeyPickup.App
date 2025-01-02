9713bbc68e1c5627ece79d4c6ae988536d1451f2
Brett Morrison <b@brettmorrison.com>
2025-01-02 11:52:13 -0800

    Styling updates for Session page

--- src/components/SessionDetails.tsx
+++ src/components/SessionDetails.tsx
@@ -1,6 +1,6 @@
 import { SessionDetailedResponse } from '@/HockeyPickup.Api';
 import { useAuth } from '@/lib/auth';
-import { ActionIcon, Group, Paper, Stack, Text, Title } from '@mantine/core';
+import { ActionIcon, Group, Paper, Text, Title } from '@mantine/core';
 import { IconPencil } from '@tabler/icons-react';
 import moment from 'moment';
 import { JSX } from 'react';
@@ -16,46 +16,42 @@ export const SessionDetails = ({ session }: SessionDetailsProps): JSX.Element =>
 
   return (
     <Paper shadow='sm' p='md'>
-      <Group justify='space-between' mb='md'>
-        <Group>
-          <Title order={3}>
-            {moment.utc(session.SessionDate).format('dddd, MM/DD/yyyy, HH:mm')}
-          </Title>
-          {isAdmin() && (
-            <ActionIcon
-              variant='subtle'
-              onClick={() => navigate(`/sessions/${session.SessionId}/edit`)}
-              size='sm'
-            >
-              <IconPencil size={16} />
-            </ActionIcon>
-          )}
-        </Group>
-      </Group>{' '}
-      <Stack gap={2}>
-        <Group>
-          <Text>{session.Note ?? ''}</Text>
+      <Paper withBorder p='xs' bg='rgba(255, 255, 255, 0.05)'>
+        <Group justify='space-between' mb='md'>
+          <Group>
+            <Title order={3}>
+              {moment.utc(session.SessionDate).format('dddd, MM/DD/yyyy, HH:mm')}
+            </Title>
+            {isAdmin() && (
+              <ActionIcon
+                variant='subtle'
+                onClick={() => navigate(`/sessions/${session.SessionId}/edit`)}
+                size='sm'
+              >
+                <IconPencil size={16} />
+              </ActionIcon>
+            )}
+          </Group>
         </Group>
         <Group>
-          <Text fw={700}>
-            Buy Window
-            {user?.PreferredPlus
-              ? ' (Preferred Plus): '
-              : user?.Preferred
-                ? ' (Preferred): '
-                : ': '}
-            {moment
-              .utc(
-                user?.PreferredPlus
-                  ? session.BuyWindowPreferredPlus
-                  : user?.Preferred
-                    ? session.BuyWindowPreferred
-                    : session.BuyWindow,
-              )
-              .format('dddd, MM/DD/yyyy, HH:mm')}
-          </Text>{' '}
+          <Text style={{ whiteSpace: 'pre-wrap' }}>{session.Note ?? ''}</Text>
         </Group>
-      </Stack>
+      </Paper>
+      <Group mt='md'>
+        <Text fw={700}>
+          Buy Window
+          {user?.PreferredPlus ? ' (Preferred Plus): ' : user?.Preferred ? ' (Preferred): ' : ': '}
+          {moment
+            .utc(
+              user?.PreferredPlus
+                ? session.BuyWindowPreferredPlus
+                : user?.Preferred
+                  ? session.BuyWindowPreferred
+                  : session.BuyWindow,
+            )
+            .format('dddd, MM/DD/yyyy, HH:mm')}
+        </Text>
+      </Group>
     </Paper>
   );
 };
