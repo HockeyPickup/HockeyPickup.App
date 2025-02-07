b7dfae0ab41483fd30e648c58a0dc4a54e1ceee8
Brett Morrison <b@brettmorrison.com>
2025-02-06 19:44:56 -0800

    Updated YouTube search link for better compatability on desktop and mobile

--- src/lib/overrides.ts
+++ src/lib/overrides.ts
@@ -1,5 +1,5 @@
 export const YOUTUBE_NAME_OVERRIDES: Record<string, string> = {
-  'Darin St. Ivany': 'Darin',
+  'Darin St. Ivany': '"Darin"',
   'Oscar Sackaroff': '"Oscar" OR "O. Sackaroff"',
   'Jack Coons': '"Jack" OR "J. Coons"',
   'Brett Morrison': '"Brett" OR "Morrison"',
